5aaaf7f67637cef2d09956f130aa2d88f6851871
Jetmir-Al <jetmiralshiqi0@gmail.com>
2026-04-07 20:55:21 +0200

    Installed some packages for server

---
+++ JesterTech.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JesterTech.Server.Data
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- JesterTech.Server/Program.cs
+++ JesterTech.Server/Program.cs
@@ -1,28 +1,71 @@
+using JesterTech.Server.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+
+
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "JesterTechPolicy",
+                      policy =>
+                      {
+                          policy.WithOrigins("")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod()
+                                .AllowCredentials();
+                      });
+}
+    );
+
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "UserSessionCookie";
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.SameSite = SameSiteMode.None;
+        options.ExpireTimeSpan = TimeSpan.FromDays(6);
+        options.SlidingExpiration = true;
+    });
 
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.MapStaticAssets();
 
-// Configure the HTTP request pipeline.
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+
+app.UseCors("LibrariaPolicy");
+
+
+
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
 
-app.MapFallbackToFile("/index.html");
+
 
 app.Run();
