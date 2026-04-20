daf4b73084212486b7b5d78d30f96a6065b2967b
Jetmir-Al <jetmiralshiqi0@gmail.com>
2026-04-20 19:36:08 +0200

    Updated Product Controller

--- JesterTech.Server/Controllers/ProductController.cs
+++ JesterTech.Server/Controllers/ProductController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JesterTech.Server.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JesterTech.Server.Controllers
@@ -7,5 +7,31 @@ namespace JesterTech.Server.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductRepository _productRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductController(IProductRepository productRepository, IWebHostEnvironment webHostEnvironment)
+        {
+            _productRepository = productRepository;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet("products")]
+        public IActionResult GetProducts()
+        {
+            var products = _productRepository.GetAllProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("products/{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
     }
 }
